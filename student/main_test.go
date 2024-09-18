948a96d50564617a2d7df92547402ea9c938fc19
nymaina <annmaina614@gmail.com>
2024-09-18 15:14:14 +0300

    Add more test cases to my test file

--- student/main_test.go
+++ student/main_test.go
@@ -6,14 +6,18 @@ import (
 
 func TestGuessIt(t *testing.T) {
 	tests := []struct {
-		input    []float64
+		input         []float64
 		expectedLower int
 		expectedUpper int
 	}{
-		{[]float64{1.5, 2.5, 3.5, 4.5}, 1, 5}, 
+		{[]float64{1.5, 2.5, 3.5, 4.5}, 1, 5},
 		{[]float64{10, 20, 30, 40, 50}, 2, 58},
+		{[]float64{5, 5, 5, 5, 5}, 5, 5},
+		{[]float64{1, 2, 3, 4, 5}, 0, 6},
+		{[]float64{-1, -2, -4, -9}, -10, 2},
+		{[]float64{100, 200, 300, 400, 500}, 17, 583},
 	}
-
+	
 	for _, test := range tests {
 		lower, upper := guessit(test.input)
 		if lower != test.expectedLower || upper != test.expectedUpper {
@@ -21,3 +25,5 @@ func TestGuessIt(t *testing.T) {
 		}
 	}
 }
+
+// {[]float64{0, 0}, math.NaN(), math.NaN()},
